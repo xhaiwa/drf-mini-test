1380479884820a9be8911999533c38fa822b54b0
xhaiwa <clement.jouenne.pro@gmail.com>
2025-09-30 11:40:38 +0200

    Book ModelView

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
-from .views import health
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import health, BookViewSet
 
+# Router DRF pour les ViewSets (CRUD automatique)
+router = DefaultRouter()
+router.register(r'books', BookViewSet)
+
+# Combiner routes automatiques + route personnalisée
 urlpatterns = [
-    path('health/', health)
+    path('health/', health),           # route personnalisée
+    path('', include(router.urls)),    # routes CRUD automatiques
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import viewsets
+from .models import Book
+from .serializers import BookSerializer
 
 @api_view(['GET'])
 def health(request):
     return Response({"status": "ok"})
+
+class BookViewSet(viewsets.ModelViewSet): # Viewset = CRUD automatique
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
