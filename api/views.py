e2eb4758d16d20b239cc2e47a8e1d0ce5f5307f5
xhaiwa <clement.jouenne.pro@gmail.com>
2025-09-30 14:07:41 +0200

    Ajout du cache dans Django

--- api/views.py
+++ api/views.py
@@ -1,17 +1,35 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import viewsets
+from django.core.cache import cache
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from .models import Book, Author
 from .serializers import BookSerializer, AuthorSerializer
 
 @api_view(['GET'])
 def health(request):
-    return Response({"status": "ok"})
+    cache.set('status', 'ok', 60)
+    return Response({"status": cache.get('status')})
 
 class BookViewSet(viewsets.ModelViewSet): # Viewset = CRUD automatique
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+    # cache de la liste
+    @method_decorator(cache_page(60), name='list')
+
+    # cache GET :id
+    @method_decorator(cache_page(60 * 2), name='retrieve')
+
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+
+    @method_decorator(cache_page(60), name='list')
+    @method_decorator(cache_page(60 * 2), name='retrieve')
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
